17fc0f14bbaaf211c3bc76063708938136c850c5
Adam O'Hern <adam@mechanicalcolor.com>
2017-06-05 17:25:57 -0400

    1.2.4 removed tagger references (errant)

--- lxserv/bling_prefsSetUserPresetsPath.py
+++ lxserv/bling_prefsSetUserPresetsPath.py
@@ -1,11 +1,11 @@
 #python
 
-import lx, lxu, modo, tagger, traceback
+import lx, lxu, modo, traceback
 
 class CommandClass(lxu.command.BasicCommand):
 
     def basic_Execute(self, msg, flags):
-        target = modo.dialogs.dirBrowse(tagger.LABEL_CHOOSE_FOLDER)
+        target = modo.dialogs.dirBrowse("Choose Folder")
         lx.eval('user.value bling_matcap_directory {%s}' % target)
 
 lx.bless(CommandClass, "bling.prefsSetUserPresetsPath")
